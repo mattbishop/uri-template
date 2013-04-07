5ff1b15ae2c5203d01cc2efe7630e6ce4fd58287
Francis Galiegue <fgaliegue@gmail.com>
2013-04-07 22:12:52 +0200

    Create a level 2 renderer

---
+++ src/main/java/com/github/fge/uritemplate/render/Level2Renderer.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2013, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.fge.uritemplate.render;
+
+public final class Level2Renderer
+    extends Renderer
+{
+    private final String expanded;
+
+    public Level2Renderer(final String value)
+    {
+        expanded = expandString(value, CharMatchers.RESERVED_PLUS_UNRESERVED);
+    }
+
+    @Override
+    public String render()
+    {
+        return expanded;
+    }
+}
